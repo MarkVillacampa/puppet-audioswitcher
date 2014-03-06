e93e330c65b2c634866a13903729d528cbdce38b
Mark Villacampa <markvjal@gmail.com>
2014-03-06 20:40:53 +0100

    fix link, not it points to macupdate's server since the original author link is broken

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include audioswitcher
 class audioswitcher {
   package { 'AudioSwitcher':
-    provider => 'compressed_app',
-    source   => 'http://spike.internal.thethurmans.com/wp-content/uploads/2014/02/AudioSwitcher-1.5.1.zip'
+    provider => 'appdmg_eula',
+    source   => 'https://www.macupdate.com/download/26433/audioswitcher-1.5.1.dmg'
   }
 }
